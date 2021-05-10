267839e965eaeceff743af1d5be81c4ab51f4461
Tamsauce <thughes78@gmail.com>
2021-05-10 15:02:47 -0700

    new attempts on delete and image

--- js/app.js
+++ js/app.js
@@ -32,36 +32,29 @@ function preventMyDefault(event){
 }
 
 const addPerson = person => {
+        const imageTag = document.createElement('img')
         const displayInput = document.createElement('li')
-        //tried changing these to i tags verses spans but there didn't seem to be a difference 
         const trashCan = document.createElement('span')
         const editTab = document.createElement('span')
-        // const imageTag = document.createElement('img')
+        
         const container = document.querySelector('#addPerson')
         
-       
+        imageTag.src = `${person.zodiacSign}`
         displayInput.innerText = `${person.name} ${horoscopeResults()}`
-        // imageTag.src = `${person.zodiacSign}`
+        
 
         
-        
-        // trashCan.innerHTML = <i class="fas fa-trash-alt"></i>
-
-    
-
-        trashCan.className = "fas fa-trash-alt delete"
-        
-        
+        trashCan.className = "delete fas fa-trash-alt"
         trashCan.innerHTML = '<i class="fa fa-trash-o" aria-hidden="true"></i>'
+        console.log(trashCan)
     
-        
         editTab.innerHTML = `<i class="fa fa-pencil" aria-hidden="true"></i>`
         editTab.className= 'fas fa-pencil-alt edit'
         
         displayInput.setAttribute('data-index',listOfPeople.length)
 
         container.append(displayInput)
-        // container.append(imageTag)
+        container.append(imageTag)
         displayInput.append(trashCan)
         displayInput.append(editTab)
     
@@ -71,7 +64,7 @@ function createPerson(nameInput, birthdateInput) {
     const newPerson = {
         name: nameInput,
         birthdate: birthdateInput,
-        zodiacSign: "aries"
+        zodiacSign: "/images/aries2.jpg"
         
     }
     console.log(newPerson.birthdate)
@@ -87,7 +80,7 @@ function createPerson(nameInput, birthdateInput) {
 
 const horoscopeImage = zodiacSign => {
     const horoscopes = { 
-        aries :      "/images/aries.jpg",
+        aries :      "/images/aries2.jpg",
         taurus :     "images/taurus2.jpg",
         gemini :     "/images/gemini2.jpg",
         cancer :     "/images/cancer2.jpg",
@@ -107,51 +100,44 @@ const horoscopeImage = zodiacSign => {
 
 
 function removePerson(event){
-    if (event.target.classList.contains("svg-inline--fa fa-trash-alt fa-w-14")){
-        console.log("Ouch! Why did you delete me?  Did you not like what your future holds?")
         let deletePerson = event.target
         deletePerson.parentElement.remove()
-        listOfPeople.shift()
-       
-        
-    }
- 
-    console.log("Delete is working")
+        listOfPeople.splice(deletePerson.parentElement.dataset.index,1)
+        console.log("Ouch! Why did you delete me?  Did you not like what your future holds?")  
 }
 
 function updateResults(event){
-    if (event.target.classList.contains("edit")){
-        console.log('did you want to edit me?')
+    //let editPerson = event.target
+    console.log("Oh no! Do you need to make a change?")  
     
     
 
-        // Get the modal
-        var modal = document.querySelector(".myModal");
+        // // Get the modal
+        // var modal = document.querySelector(".myModal");
 
-        // // Get the button that opens the modal
-        // var btn = document.getElementById("myBtn");
+        // // // Get the button that opens the modal
+        // // var btn = document.getElementById("myBtn");
 
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
+        // // Get the <span> element that closes the modal
+        // var span = document.getElementsByClassName("close")[0];
 
-        // When the user clicks on the button, open the modal
-        editTab.onclick = function() {
-        modal.style.display = "block";
-        }
+        // // When the user clicks on the button, open the modal
+        // editTab.onclick = function() {
+        // modal.style.display = "block";
+        // }
 
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-        modal.style.display = "none";
-        }
+        // // When the user clicks on <span> (x), close the modal
+        // span.onclick = function() {
+        // modal.style.display = "none";
+        // }
 
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-  }
+        // // When the user clicks anywhere outside of the modal, close it
+        // window.onclick = function(event) {
+        // if (event.target == modal) {
+        //     modal.style.display = "none";
+  //}
 }        
-    }
-}
+  
  
  
 
